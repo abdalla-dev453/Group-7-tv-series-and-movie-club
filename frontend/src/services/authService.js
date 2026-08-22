11b51bdd54d2c3e88ae355ded27f23fb8f0b91fb
cory <cory.martel@student.moringaschool.com>
2026-08-22 11:39:56 +0300

    modified files

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,7 +3,4 @@ import api from './api';
 export const signup = async (payload) => api.post('/signup', payload);
 
 export const login = async (payload) => api.post('/login', payload);
-
 export const logout = async () => api.post('/logout');
-
-export const getCurrentUser = async () => api.get('/current-user');
