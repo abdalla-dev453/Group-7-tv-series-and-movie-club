b32f2ed275f1e3f17d1580d83678a55c3e131b15
alekkoech <koechalek@gmail.com>
2026-08-25 11:02:23 +0300

    updated joinclubbutton

--- frontend/src/pages/Clubs/JoinClubButton.jsx
+++ frontend/src/pages/Clubs/JoinClubButton.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Button from '../common/Button';
+import Button from "../../components/Button";
 import { joinClub, leaveClub } from '../../services/clubService';
 import './JoinClubButton.css';
 
